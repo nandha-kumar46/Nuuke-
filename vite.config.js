13929aab8177a48a3478812880f92129dc566583
nandha-kumar46 <nandha@mervice.in>
2025-08-29 18:20:02 +0530

    added 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/'
+  // If deploying to a custom domain, keep '/'
+  // If deploying to GitHub Pages under a repo, replace with '/<repo-name>/'
+  base: '/'
 })
